8d07b154a7b9cac8c95bf64f4f8f593d5207d742
George Clarke <flaredude2@hotmail.com>
2012-10-29 23:38:46 +0100

    Updated thy2thine test to be more concise.

--- thy2thinetester.php
+++ thy2thinetester.php
@@ -4,32 +4,31 @@
 */
 
 $comment = "Hello world, thy ease is mine."; // will be the user submitted one from the json. 
-$findThy = 'thy';
 
-$explodeString = explode($findThy,$comment); // This splits string into two and where [1] begins with the desired word after thy.
+$thyPattern = "/[T]hy [aeiouh]/i"; // finds if offending word exists.
 
-if($explodeString){
-	$explodeString1 = explode(" ",$explodeString[1]);
-	$wordAfterThy = $explodeString1[1]; // contains offending word.
+
+$offenseExists = preg_match($thyPattern,$comment,$matches);
+$findThyCase = "/[t]hy/i"; // Grabs case of thy: thy Thy THY. 
+$findThyCaseSrch = preg_match($findThyCase,$comment,$matches); 
+$explodingThy = $matches[0]; // Saves found thy as var. 
+
+// Updates needed: if there is more than one offending thy. 
+
+if($offenseExists){
+	$explodeString = explode($explodingThy,$comment); // Splits string at the found Thy.
+	$findOffendingWord = $explodeString[1]; // Saves 2nd part of split beginning with offending word. 
+	$findOffendingWord = explode(" ",$findOffendingWord); // explodes further to get offending word.
+	$offendingWord = $findOffendingWord[1]; // Saves offending word in var. 
+	$letter = substr($offendingWord,0,1); // Gets first letter of offending word for use in comment. 
 	
-	// Now the word needs to be checked if it contains a vowel via regex.
-	
-	$firstLetter = substr($wordAfterThy,0,1);
-	$regexVowel = "/[aeiouhAEIOUH]/";
-	
-	// Find out if first letter is vowel via regex.
-	$isVowel = preg_match($regexVowel,$firstLetter,$matches);
-	if($isVowel){
-		echo "<p> <i>'Quoted text goes here: ".$comment."'</i>
+	echo "<p> <i>'Quoted text goes here: ".$comment."'</i>
 		<p>Hi there. I couldn't help but notice you've made the mistake of placing 'thy' before a vowel.
 				<br> It changes to 'thine' before a word beginning with A E I O U and H. <br>
-				But no biggie. I'll correct it for you: thy <b>".$wordAfterThy."</b> begins with the vowel
-				'".$firstLetter."', hence it should be: <b>thine ".$wordAfterThy."</b>. Thank you for listening.";
-	} else {
-		echo "Hmm something went wrong.";
-	}
+				But no biggie. I'll correct it for you: thy <b>".$offendingWord."</b> begins with the vowel
+				'".$letter."', hence it should be: <b>thine ".$offendingWord."</b>. Thank you for listening.";
+	
 } else {
-	echo "nothing mate";
-} 
-
+	echo "No findy";
+}
 ?>
