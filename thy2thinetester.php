a1c8613a425a6a9224a91b82a92f66cdf522fc13
George Clarke <flaredude2@hotmail.com>
2012-10-30 18:52:09 +0100

    These are the most recent copies and conform to what was said in last message

--- thy2thinetester.php
+++ thy2thinetester.php
@@ -3,31 +3,67 @@
 *	Just a brief test of using regex to see if thy thine is right.
 */
 
-$comment = "Hello world, thy ease is mine. Thy hearing is fine. "; // will be the user submitted one from the json. 
+$comment = "Hello world, thy ease is mine.  hearing is fine. "; // will be the user submitted one from the json. 
 
-$thyPattern = "/[T]hy [aeiouh]/i"; // finds if offending word exists.
+$thyPattern = "/[T]hy [aeiouh]/i"; // finds if offending word exists: thy + vowel. Case insensitive.
+$thyCasePattern = "/[t]hy/i"; // Grabs case of thy: thy Thy THY. 
 
-
-$offenseExists = preg_match($thyPattern,$comment,$matches);
-$findThyCase = "/[t]hy/i"; // Grabs case of thy: thy Thy THY. 
-$findThyCaseSrch = preg_match($findThyCase,$comment,$matches); 
-$explodingThy = $matches[0]; // Saves found thy as var. 
+$offenseExists = preg_match_all($thyPattern,$comment,$matches); // finds if offense exists. 
+$count = count($matches[0]); // Count to see if more than 1 thy.
 
 // Updates needed: if there is more than one offending thy. 
+// Begin for else block to determine if there is more than one thy. 
 
 if($offenseExists){
-	$explodeString = explode($explodingThy,$comment); // Splits string at the found Thy.
+	$findThyCaseSrch = preg_match_all($thyCasePattern,$comment,$matches);
+	if($count>1){
+		// loop through elements and save as var
+		foreach($matches[0] as $key=>$value){ // saves each thy to its own var. 
+			$foundThy[$key] = $value;
+		}
+		$i = 0;
+		$keyCount = $count - 1; // changes count's value to match array elements starting from 0
+		
+		while($i<=$keyCount){ // while $i is less than the number of elements in $matches array
+			$explodeString = explode($foundThy[$i],$comment); // make exploded arrays
+			$i++;
+			echo $explodeString[1];
+		}
+		
+	} else {
+		// save single element as var.
+		$foundThy = $matches[0][0];
+		$explodeString = explode($foundThy,$comment);
+	}
+	
+	// Update this block to allow for multiple thy problems. ATM, only allows for 1. 
+	
+	// $explodeString = explode($foundThy,$comment); // Splits string at the found Thy.
 	$findOffendingWord = explode(" ",$explodeString[1]); // explodes further to get offending word.
 	$offendingWord = $findOffendingWord[1]; // Saves offending word in var. 
 	$letter = substr($offendingWord,0,1); // Gets first letter of offending word for use in comment. 
-	
-	echo "<p> <i>'Quoted text goes here: ".$comment."'</i>
-		<p>Hi there. I couldn't help but notice you've made the mistake of placing 'thy' before a vowel.
-				<br> It changes to 'thine' before a word beginning with A E I O U and H. <br>
-				But no biggie. I'll correct it for you: thy <b>".$offendingWord."</b> begins with the vowel
-				'".$letter."', hence it should be: <b>thine ".$offendingWord."</b>. Thank you for listening.";
+	// Use reddit comment markup. 
+	echo "<p> '<i>$comment</i>'
+
+	<p>Hi there! I couldn't help but notice that you've made the mistake of placing
+	ye olde Englishe word 'thy' before a word beginning with a vowel (A E I O U, 
+	and including H) - hence it should rather be 'thine'. 
+
+	<p>In your case, you have written '<i>thy $offendingWord</i>' when it should be '<i>thine $offendingWord</i>'.
+	You can see that the word '<i>$offendingWord</i>' begins with the vowel '<i>$letter</i>', and 
+	so, follows the aforementioned rule. 
+
+	<p>Hopefully that was helpful for you! Interested in learning more? Why not check
+	out <a href='http://www.reddit.com/r/linguistics'>r/Linguistics</a>! Want to read more
+	on thy/thine? Go <a href='http://en.wikipedia.org/wiki/Middle_English#Pronouns'>here</a>! (Middle english made the distinction between
+	singular and plural 'you' - whereas today we use only the one form. Also
+	that th sound is actually a single letter - it's called the <a href='http://en.wikipedia.org/wiki/Thorn_%28letter%29'>thorn</a>. It's still
+	in use in Icelandic - which is a language that has in fact many old features
+	still retained.)";
 	
 } else {
-	echo "No findy";
+	echo "Nothing found. It's all good!";
 }
 ?>
+
+
